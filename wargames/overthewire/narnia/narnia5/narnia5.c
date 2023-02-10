69f78aa0516da0e4441576227ecb1950571ed7ca
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-10 05:09:53 +0100

    Create narnia5.c

---
+++ wargames/overthewire/narnia/narnia5/narnia5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+        int i = 1;
+        char buffer[64];
+
+        snprintf(buffer, sizeof buffer, argv[1]);
+        buffer[sizeof (buffer) - 1] = 0;
+        printf("Change i's value from 1 -> 500. ");
+
+        if(i==500){
+                printf("GOOD\n");
+        setreuid(geteuid(),geteuid());
+                system("/bin/sh");
+        }
+
+        printf("No way...let me give you a hint!\n");
+        printf("buffer : [%s] (%d)\n", buffer, strlen(buffer));
+        printf ("i = %d (%p)\n", i, &i);
+        return 0;
+}
