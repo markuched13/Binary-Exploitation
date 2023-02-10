0424001e190d0b4177fe7f6d59a36e6a3808de52
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-02-10 03:27:14 +0100

    Create narnia3.c

---
+++ wargames/overthewire/narnia/narnia3/narnia3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+
+    int  ifd,  ofd;
+    char ofile[16] = "/dev/null";
+    char ifile[32];
+    char buf[32];
+
+    if(argc != 2){
+        printf("usage, %s file, will send contents of file 2 /dev/null\n",argv[0]);
+        exit(-1);
+    }
+
+    /* open files */
+    strcpy(ifile, argv[1]);
+    if((ofd = open(ofile,O_RDWR)) < 0 ){
+        printf("error opening %s\n", ofile);
+        exit(-1);
+    }
+    if((ifd = open(ifile, O_RDONLY)) < 0 ){
+        printf("error opening %s\n", ifile);
+        exit(-1);
+    }
+
+    /* copy from file1 to file2 */
+    read(ifd, buf, sizeof(buf)-1);
+    write(ofd,buf, sizeof(buf)-1);
+    printf("copied contents of %s to a safer place... (%s)\n",ifile,ofile);
+
+    /* close 'em */
+    close(ifd);
+    close(ofd);
+
+    exit(1);
+}
